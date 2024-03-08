228c5d8909c250d82f7b06a9ce15070a3674797e
SathishMadesh <s747272946@gmail.com>
2024-03-08 22:11:46 +0530

    update

--- anagrams.js
+++ anagrams.js
@@ -5,7 +5,7 @@ let str2 = "Army";
 
 const strArr1 = str1.toLowerCase().split('').sort().join('');
 const strArr2 = str2.toLowerCase().split('').sort().join('');
-
+ 
 if(strArr1 === strArr2){
     console.log("true");
 } else {
