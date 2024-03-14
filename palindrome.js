5abba41dfb2882455a79ef1a477a7b393029c50e
SathishMadesh <s747272946@gmail.com>
2024-03-14 22:23:19 +0530

    'update'

--- palindrome.js
+++ palindrome.js
@@ -16,8 +16,23 @@ function palindrome(x){
         right--;
     }
     return true
-} 
+}
 
 console.log(palindrome(12321));
 
 console.log(palindrome("hello"));
+
+function palindrome1(x){
+    x = x.toString();
+    rev_x = x.split('').reverse().join('');
+    if (x < 0){
+        console.log(x," is not a palindrom");
+    } else if (x === rev_x){
+        console.log(x," is palindrom");
+    } else {
+        console.log(x, "is not a palindrom");
+    }
+}
+
+palindrome1(12321);
+palindrome1("hello");
