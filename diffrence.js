049c58feb1e6de8c22ef29cfc6f561bac2a85869
SathishMadesh <s747272946@gmail.com>
2024-03-16 10:53:47 +0530

    update

---
+++ diffrence.js
@@ -0,0 +1,22 @@
+// pair the two values from the given array that has the diffrence of K=4
+// given array : [9,6,5,2,1]
+// output : (9,5)(6,2)(5,1)
+
+function diffrence(arr){
+    const k=4;
+    result = [];
+    for (let i=0; i < arr.length; i++){
+        //console.log(arr[i]);
+        for (let j=i; j < arr.length; j++){
+            // console.log(arr[i],arr[j]);
+            if (Math.abs(arr[i]-arr[j]) === k){
+                result.push([arr[i],arr[j]]);
+                
+            }
+        }
+        
+    }
+    return result;
+}
+
+console.log(diffrence([9,6,5,2,1,10]));
