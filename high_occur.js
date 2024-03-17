3c333439723761597d7b674f53ddc96236e38fc1
SathishMadesh <s747272946@gmail.com>
2024-03-17 23:17:15 +0530

    update

--- high_occur.js
+++ high_occur.js
@@ -6,6 +6,17 @@ function occurence(arr){
     let counts = {};
     let maxCount = 0;
     let mostRepeatedNum;
+
+    for (let num of arr){
+        counts[num] = (counts[num] || 0) + 1;
+        if (counts[num] >= maxCount) {
+            maxCount = counts[num];
+            if (mostRepeatedNum === undefined || num > mostRepeatedNum){
+                mostRepeatedNum = num;
+            }
+        }
+    }
+    return mostRepeatedNum;
 }
 
-console.log(occurence([1,1,1,2,2,3,3,5,5,5]));
+console.log(occurence([1,1,1,2,2,3,3,5,5,5,6,6,6]));
